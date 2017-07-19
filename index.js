57399b066dada245e08e778014e7cfc84ff14934
buffpojken <daniel@sykewarrior.com>
2017-07-19 16:12:04 +0200

    tweaking order-of-ops to provide fileType-set mime to callbacks

--- index.js
+++ index.js
@@ -39,20 +39,23 @@ function autoContentType (req, file, cb) {
 }
 
 function collect (storage, req, file, cb) {
-  parallel([
-    storage.getBucket.bind(storage, req, file),
-    storage.getKey.bind(storage, req, file),
-    storage.getAcl.bind(storage, req, file),
-    storage.getMetadata.bind(storage, req, file),
-    storage.getCacheControl.bind(storage, req, file),
-    storage.getContentDisposition.bind(storage, req, file),
-    storage.getStorageClass.bind(storage, req, file),
-    storage.getSSE.bind(storage, req, file),
-    storage.getSSEKMS.bind(storage, req, file)
-  ], function (err, values) {
+
+  storage.getContentType(req, file, function (err, contentType, replacementStream) {
     if (err) return cb(err)
 
-    storage.getContentType(req, file, function (err, contentType, replacementStream) {
+
+    file.calculatedMime = contentType;
+    parallel([
+      storage.getBucket.bind(storage, req, file),
+      storage.getKey.bind(storage, req, file),
+      storage.getAcl.bind(storage, req, file),
+      storage.getMetadata.bind(storage, req, file),
+      storage.getCacheControl.bind(storage, req, file),
+      storage.getContentDisposition.bind(storage, req, file),
+      storage.getStorageClass.bind(storage, req, file),
+      storage.getSSE.bind(storage, req, file),
+      storage.getSSEKMS.bind(storage, req, file)
+    ], function (err, values) {
       if (err) return cb(err)
 
       cb.call(storage, null, {
@@ -67,9 +70,43 @@ function collect (storage, req, file, cb) {
         replacementStream: replacementStream,
         serverSideEncryption: values[7],
         sseKmsKeyId: values[8]
-      })
-    })
-  })
+      });
+
+    });
+  });
+
+  // parallel([
+  //   storage.getBucket.bind(storage, req, file),
+  //   storage.getKey.bind(storage, req, file),
+  //   storage.getAcl.bind(storage, req, file),
+  //   storage.getMetadata.bind(storage, req, file),
+  //   storage.getCacheControl.bind(storage, req, file),
+  //   storage.getContentDisposition.bind(storage, req, file),
+  //   storage.getStorageClass.bind(storage, req, file),
+  //   storage.getSSE.bind(storage, req, file),
+  //   storage.getSSEKMS.bind(storage, req, file)
+  // ], function (err, values) {
+  //   if (err) return cb(err)
+
+  //   storage.getContentType(req, file, function (err, contentType, replacementStream) {
+  //     if (err) return cb(err)
+
+  //     cb.call(storage, null, {
+  //       bucket: values[0],
+  //       key: values[1],
+  //       acl: values[2],
+  //       metadata: values[3],
+  //       cacheControl: values[4],
+  //       contentDisposition: values[5],
+  //       storageClass: values[6],
+  //       contentType: contentType,
+  //       replacementStream: replacementStream,
+  //       serverSideEncryption: values[7],
+  //       sseKmsKeyId: values[8]
+  //     })
+  //   })
+
+  // })
 }
 
 function S3Storage (opts) {
