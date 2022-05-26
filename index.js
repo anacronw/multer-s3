24c14cc3ba59e7becf127d5276996074096a4e09
Catriel Guillen <catrielguillen@gmail.com>
2022-05-26 14:29:43 -0400

    style(switch): change styles for switch statements

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var svgRegex = /^\s*(?:<\?xml[^>]*>\s*)?(?:<!doctype svg[^>]*>\s*)?<svg[^>]*>/i
 
 function isSvg (svg) {
   // Remove DTD entities
-  svg = svg.replace(/\s*<!Entity\s+\S*\s*(?:"|')[^"]+(?:"|')\s*>/gim, '')
+  svg = svg.replace(/\s*<!Entity\s+\S*\s*(?:"|')[^"]+(?:"|')\s*>/img, '')
   // Remove DTD markup declarations
   svg = svg.replace(/\[?(?:\s*<![A-Z]+[^>]*>\s*)*\]?/g, '')
   // Remove HTML comments
@@ -65,195 +65,119 @@ function autoContentType (req, file, cb) {
 }
 
 function collect (storage, req, file, cb) {
-  parallel(
-    [
-      storage.getBucket.bind(storage, req, file),
-      storage.getKey.bind(storage, req, file),
-      storage.getAcl.bind(storage, req, file),
-      storage.getMetadata.bind(storage, req, file),
-      storage.getCacheControl.bind(storage, req, file),
-      storage.getContentDisposition.bind(storage, req, file),
-      storage.getStorageClass.bind(storage, req, file),
-      storage.getSSE.bind(storage, req, file),
-      storage.getSSEKMS.bind(storage, req, file),
-      storage.getContentEncoding.bind(storage, req, file)
-    ],
-    function (err, values) {
+  parallel([
+    storage.getBucket.bind(storage, req, file),
+    storage.getKey.bind(storage, req, file),
+    storage.getAcl.bind(storage, req, file),
+    storage.getMetadata.bind(storage, req, file),
+    storage.getCacheControl.bind(storage, req, file),
+    storage.getContentDisposition.bind(storage, req, file),
+    storage.getStorageClass.bind(storage, req, file),
+    storage.getSSE.bind(storage, req, file),
+    storage.getSSEKMS.bind(storage, req, file),
+    storage.getContentEncoding.bind(storage, req, file)
+  ], function (err, values) {
+    if (err) return cb(err)
+
+    storage.getContentType(req, file, function (err, contentType, replacementStream) {
       if (err) return cb(err)
 
-      storage.getContentType(req, file, function (err, contentType, replacementStream) {
-        if (err) return cb(err)
-
-        cb.call(storage, null, {
-          bucket: values[0],
-          key: values[1],
-          acl: values[2],
-          metadata: values[3],
-          cacheControl: values[4],
-          contentDisposition: values[5],
-          storageClass: values[6],
-          contentType: contentType,
-          replacementStream: replacementStream,
-          serverSideEncryption: values[7],
-          sseKmsKeyId: values[8],
-          contentEncoding: values[9]
-        })
+      cb.call(storage, null, {
+        bucket: values[0],
+        key: values[1],
+        acl: values[2],
+        metadata: values[3],
+        cacheControl: values[4],
+        contentDisposition: values[5],
+        storageClass: values[6],
+        contentType: contentType,
+        replacementStream: replacementStream,
+        serverSideEncryption: values[7],
+        sseKmsKeyId: values[8],
+        contentEncoding: values[9]
       })
-    }
-  )
+    })
+  })
 }
 
 function S3Storage (opts) {
   switch (typeof opts.s3) {
-    case 'object':
-      this.s3 = opts.s3
-      break
-    default:
-      throw new TypeError('Expected opts.s3 to be object')
+    case 'object': this.s3 = opts.s3; break
+    default: throw new TypeError('Expected opts.s3 to be object')
   }
 
   switch (typeof opts.bucket) {
-    case 'function':
-      this.getBucket = opts.bucket
-      break
-    case 'string':
-      this.getBucket = staticValue(opts.bucket)
-      break
-    case 'undefined':
-      throw new Error('bucket is required')
-    default:
-      throw new TypeError('Expected opts.bucket to be undefined, string or function')
+    case 'function': this.getBucket = opts.bucket; break
+    case 'string': this.getBucket = staticValue(opts.bucket); break
+    case 'undefined': throw new Error('bucket is required')
+    default: throw new TypeError('Expected opts.bucket to be undefined, string or function')
   }
 
   switch (typeof opts.key) {
-    case 'function':
-      this.getKey = opts.key
-      break
-    case 'undefined':
-      this.getKey = defaultKey
-      break
-    default:
-      throw new TypeError('Expected opts.key to be undefined or function')
+    case 'function': this.getKey = opts.key; break
+    case 'undefined': this.getKey = defaultKey; break
+    default: throw new TypeError('Expected opts.key to be undefined or function')
   }
 
   switch (typeof opts.acl) {
-    case 'function':
-      this.getAcl = opts.acl
-      break
-    case 'string':
-      this.getAcl = staticValue(opts.acl)
-      break
-    case 'undefined':
-      this.getAcl = defaultAcl
-      break
-    default:
-      throw new TypeError('Expected opts.acl to be undefined, string or function')
+    case 'function': this.getAcl = opts.acl; break
+    case 'string': this.getAcl = staticValue(opts.acl); break
+    case 'undefined': this.getAcl = defaultAcl; break
+    default: throw new TypeError('Expected opts.acl to be undefined, string or function')
   }
 
   switch (typeof opts.contentType) {
-    case 'function':
-      this.getContentType = opts.contentType
-      break
-    case 'undefined':
-      this.getContentType = defaultContentType
-      break
-    default:
-      throw new TypeError('Expected opts.contentType to be undefined or function')
+    case 'function': this.getContentType = opts.contentType; break
+    case 'undefined': this.getContentType = defaultContentType; break
+    default: throw new TypeError('Expected opts.contentType to be undefined or function')
   }
 
   switch (typeof opts.metadata) {
-    case 'function':
-      this.getMetadata = opts.metadata
-      break
-    case 'undefined':
-      this.getMetadata = defaultMetadata
-      break
-    default:
-      throw new TypeError('Expected opts.metadata to be undefined or function')
+    case 'function': this.getMetadata = opts.metadata; break
+    case 'undefined': this.getMetadata = defaultMetadata; break
+    default: throw new TypeError('Expected opts.metadata to be undefined or function')
   }
 
   switch (typeof opts.cacheControl) {
-    case 'function':
-      this.getCacheControl = opts.cacheControl
-      break
-    case 'string':
-      this.getCacheControl = staticValue(opts.cacheControl)
-      break
-    case 'undefined':
-      this.getCacheControl = defaultCacheControl
-      break
-    default:
-      throw new TypeError('Expected opts.cacheControl to be undefined, string or function')
+    case 'function': this.getCacheControl = opts.cacheControl; break
+    case 'string': this.getCacheControl = staticValue(opts.cacheControl); break
+    case 'undefined': this.getCacheControl = defaultCacheControl; break
+    default: throw new TypeError('Expected opts.cacheControl to be undefined, string or function')
   }
 
   switch (typeof opts.contentDisposition) {
-    case 'function':
-      this.getContentDisposition = opts.contentDisposition
-      break
-    case 'string':
-      this.getContentDisposition = staticValue(opts.contentDisposition)
-      break
-    case 'undefined':
-      this.getContentDisposition = defaultContentDisposition
-      break
-    default:
-      throw new TypeError('Expected opts.contentDisposition to be undefined, string or function')
+    case 'function': this.getContentDisposition = opts.contentDisposition; break
+    case 'string': this.getContentDisposition = staticValue(opts.contentDisposition); break
+    case 'undefined': this.getContentDisposition = defaultContentDisposition; break
+    default: throw new TypeError('Expected opts.contentDisposition to be undefined, string or function')
   }
 
   switch (typeof opts.contentEncoding) {
-    case 'function':
-      this.getContentEncoding = opts.contentEncoding
-      break
-    case 'string':
-      this.getContentEncoding = staticValue(opts.contentEncoding)
-      break
-    case 'undefined':
-      this.getContentEncoding = defaultContentEncoding
-      break
-    default:
-      throw new TypeError('Expected opts.contentEncoding to be undefined, string or function')
+    case 'function': this.getContentEncoding = opts.contentEncoding; break
+    case 'string': this.getContentEncoding = staticValue(opts.contentEncoding); break
+    case 'undefined': this.getContentEncoding = defaultContentEncoding; break
+    default: throw new TypeError('Expected opts.contentEncoding to be undefined, string or function')
   }
 
   switch (typeof opts.storageClass) {
-    case 'function':
-      this.getStorageClass = opts.storageClass
-      break
-    case 'string':
-      this.getStorageClass = staticValue(opts.storageClass)
-      break
-    case 'undefined':
-      this.getStorageClass = defaultStorageClass
-      break
-    default:
-      throw new TypeError('Expected opts.storageClass to be undefined, string or function')
+    case 'function': this.getStorageClass = opts.storageClass; break
+    case 'string': this.getStorageClass = staticValue(opts.storageClass); break
+    case 'undefined': this.getStorageClass = defaultStorageClass; break
+    default: throw new TypeError('Expected opts.storageClass to be undefined, string or function')
   }
 
   switch (typeof opts.serverSideEncryption) {
-    case 'function':
-      this.getSSE = opts.serverSideEncryption
-      break
-    case 'string':
-      this.getSSE = staticValue(opts.serverSideEncryption)
-      break
-    case 'undefined':
-      this.getSSE = defaultSSE
-      break
-    default:
-      throw new TypeError('Expected opts.serverSideEncryption to be undefined, string or function')
+    case 'function': this.getSSE = opts.serverSideEncryption; break
+    case 'string': this.getSSE = staticValue(opts.serverSideEncryption); break
+    case 'undefined': this.getSSE = defaultSSE; break
+    default: throw new TypeError('Expected opts.serverSideEncryption to be undefined, string or function')
   }
 
   switch (typeof opts.sseKmsKeyId) {
-    case 'function':
-      this.getSSEKMS = opts.sseKmsKeyId
-      break
-    case 'string':
-      this.getSSEKMS = staticValue(opts.sseKmsKeyId)
-      break
-    case 'undefined':
-      this.getSSEKMS = defaultSSEKMS
-      break
-    default:
-      throw new TypeError('Expected opts.sseKmsKeyId to be undefined, string, or function')
+    case 'function': this.getSSEKMS = opts.sseKmsKeyId; break
+    case 'string': this.getSSEKMS = staticValue(opts.sseKmsKeyId); break
+    case 'undefined': this.getSSEKMS = defaultSSEKMS; break
+    default: throw new TypeError('Expected opts.sseKmsKeyId to be undefined, string, or function')
   }
 }
 
