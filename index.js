eb69cc70075231ee3a069f8efb26399d2ccaec3b
Thanos Valimitis <thanos.valimitis@gmail.com>
2022-05-31 14:06:56 +0300

    fix: fix _removeFile crashing due to deprecated Delete Object API

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var fileType = require('file-type')
 var htmlCommentRegex = require('html-comment-regex')
 var parallel = require('run-parallel')
 var Upload = require('@aws-sdk/lib-storage').Upload
+var DeleteObjectCommand = require('@aws-sdk/client-s3').DeleteObjectCommand;
 var util = require('util')
 
 function staticValue (value) {
@@ -241,7 +242,13 @@ S3Storage.prototype._handleFile = function (req, file, cb) {
 }
 
 S3Storage.prototype._removeFile = function (req, file, cb) {
-  this.s3.deleteObject({ Bucket: file.bucket, Key: file.key }, cb)
+  this.s3.send(
+    new DeleteObjectCommand({
+      Bucket: file.bucket,
+      Key: file.key,
+    }),
+    cb,
+  )
 }
 
 module.exports = function (opts) {
